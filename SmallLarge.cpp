e6ca07495880772abda0030b0a98665e694d7cc7
Sadman Arian <23-54986-3@student.aiub.edu>
2024-10-14 19:40:12 +0600

    Create SmallLarge.cpp

---
+++ SmallLarge.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    int smallest = arr[0], largest = arr[0];
+
+    for (int i = 1; i < n; ++i) {
+        if (arr[i] < smallest) {
+            smallest = arr[i];
+        }
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+
+    cout << "Smallest element: " << smallest << endl;
+    cout << "Largest element: " << largest << endl;
+
+    return 0;
+}
